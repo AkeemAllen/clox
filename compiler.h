eef4b98633c54576f7511dc2275818f330dbb222
Akeem Allen <akeem.allen.dev@proton.me>
2026-07-29 04:15:23 +0000

    Implemented barely working Object Strings

--- compiler.h
+++ compiler.h
@@ -2,7 +2,9 @@
 #define clox_compiler_h
 
 #include "chunk.h"
+#include "object.h"
+#include "vm.h"
 
-bool compile(const char* source, Chunk* chunk);
+bool compile(const char *source, Chunk *chunk);
 
 #endif // !clox_compiler_h
