ee6a446bd3911e248115cdbf045aeca3d533a47b
Akeem Allen <akeem.allen.dev@proton.me>
2026-09-16 03:10:07 +0000

    Garbage collector complete

--- memory.c
+++ memory.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdlib.h>
 
 #include "compiler.h"
@@ -6,16 +7,23 @@
 #include "value.h"
 #include "vm.h"
 
+#define GC_HEAP_GROW_FACTOR 2
+
 #ifdef DEBUG_LOG_GC
 #include "debug.h"
 #include <stdio.h>
 #endif
 
 void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
+  vm.bytesAllocated += newSize - oldSize;
   if (newSize > oldSize) {
 #ifdef DEBUG_STRESS_GC
     collectGarbage();
 #endif
+  }
+
+  if (vm.bytesAllocated > vm.nextGC) {
+    collectGarbage();
   }
 
   if (newSize == 0) {
@@ -180,6 +188,7 @@ static void sweep() {
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
   printf("-- gc begin\n");
+  size_t before = vm.bytesAllocated;
 #endif
 
   markRoots();
@@ -187,8 +196,12 @@ void collectGarbage() {
   tableRemoveWhite(&vm.strings);
   sweep();
 
+  vm.nextGC = vm.bytesAllocated * GC_HEAP_GROW_FACTOR;
+
 #ifdef DEBUG_LOG_GC
   printf("-- gc end\n");
+  printf("   collected %zu bytes (from %zu to %zu) next at %zu\n",
+         before - vm.bytesAllocated, before, vm.bytesAllocated, vm.nextGC);
 #endif
 }
 
